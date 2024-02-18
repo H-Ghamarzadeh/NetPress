225827728905dfca896933d4449e26e4438c4398
H-Ghamarzadeh <h.ghamarzadeh@hotmail.com>
2024-02-18 10:29:46 +0000

    Add Repository

---
+++ NetPress.Application/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetPress.Application.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(string id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(string id, T entity);
+        Task DeleteAsync(string id);
+    }
+}
---
+++ NetPress.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,41 @@
+﻿using NetPress.Application.Repository;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace NetPress.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly IMongoCollection<T> _collection;
+
+        public Repository(IMongoDatabase database, string collectionName)
+        {
+            _collection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _collection.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+            return await _collection.Find(Builders<T>.Filter.Eq("_id", new ObjectId(id))).FirstOrDefaultAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _collection.InsertOneAsync(entity);
+        }
+
+        public async Task UpdateAsync(string id, T entity)
+        {
+            await _collection.ReplaceOneAsync(Builders<T>.Filter.Eq("_id", new ObjectId(id)), entity);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _collection.DeleteOneAsync(Builders<T>.Filter.Eq("_id", new ObjectId(id)));
+        }
+    }
+}
