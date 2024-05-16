88c718e2ac5ddecd6d3914d3401eac0ee89a87f2
H-Ghamarzadeh <h.ghamarzadeh@hotmail.com>
2024-05-16 10:32:45 +0100

    UI

--- src/UI/NetPress/ViewComponents/PostsListComponent.cs
+++ src/UI/NetPress/ViewComponents/PostsListComponent.cs
@@ -5,11 +5,16 @@ namespace NetPress.ViewComponents
 {
     public class PostsListComponent : ViewComponent
     {
-        public IViewComponentResult Invoke(List<Post> posts)
+        public IViewComponentResult Invoke(List<Post> posts, int? colPerRow = 3)
         {
             if (posts.Count == 0) return Content("");
 
+            if (colPerRow <= 0) colPerRow = 1;
+            else if (colPerRow > 12) colPerRow = 12;
+            ViewData["ColPerRow"] = colPerRow;
+
             return View(model: posts);
         }
+
     }
 }
